b44eda3f762ff52851f913ab82c99a53544b175b
Gino Desangles <blushingbox@yahoo.com>
2019-04-03 20:05:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -74,7 +74,7 @@ class Person
       person.happiness -= 2
       return "blah blah partisan blah lobbyist"
     elsif topic == "weather"
-      self.happiness += 2
+      self.happiness += 1
       person.happiness += 1
       return "blah blah sun blah rain"
     else
